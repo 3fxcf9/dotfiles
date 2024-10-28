96c9895e2d7330491af9a8d67338df2ca7754cf5
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2024-10-28 12:38:15 +0100

    Kanata: map numbers to space + home-row

--- hosts/common/optional/services/kanata.nix
+++ hosts/common/optional/services/kanata.nix
@@ -3,20 +3,50 @@
     enable = true;
     keyboards.main = {
       extraDefCfg = "process-unmapped-keys yes";
+      # config = ''
+      #   (defsrc
+      #    caps
+      #   )
+      #   (defvar
+      #    tap-time 150
+      #    hold-time 200
+      #   )
+      #   (defalias
+      #    caps (tap-hold-press 150 200 esc lctl)
+      #   )
+      #
+      #   (deflayer base
+      #    @caps
+      #   )
+      # '';
       config = ''
         (defsrc
          caps
+         a s d f g h j k l ;
+         lmet spc
         )
+
         (defvar
          tap-time 150
          hold-time 200
         )
+
         (defalias
          caps (tap-hold-press 150 200 esc lctl)
+         num (layer-toggle numbers)
+         spc-num (tap-hold-press 150 200 spc @num)
         )
 
         (deflayer base
          @caps
+         a s d f g h j k l ;
+         lmet @spc-num
+        )
+
+        (deflayer numbers
+         _
+         1 2 3 4 5 6 7 8 9 0
+         _ _
         )
       '';
     };
