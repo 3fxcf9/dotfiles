dfd882ec2e02b1b9924dcda194602e00298c306d
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2024-12-27 12:08:36 +0100

    Fix Anki not launching after upgrade

--- hosts/common/optional/anki.nix
+++ hosts/common/optional/anki.nix
@@ -1,5 +1,23 @@
 {pkgs, ...}: {
-  environment.systemPackages = [
-    pkgs.anki-bin
+  environment.systemPackages = with pkgs; [
+    (writeShellScriptBin "anki" ''
+      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+        pkgs.xorg.libxshmfence
+        pkgs.libGL
+        pkgs.libGLU
+        pkgs.zlib
+        pkgs.glib
+      ]}:$LD_LIBRARY_PATH
+      exec ${anki-bin}/bin/anki "$@"
+    '')
+
+    (makeDesktopItem {
+      name = "Anki";
+      exec = "anki";
+      icon = "anki";
+      desktopName = "Anki";
+      genericName = "Flashcards";
+      categories = ["Education"];
+    })
   ];
 }
